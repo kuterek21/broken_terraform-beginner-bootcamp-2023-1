19383406069d27757e849bfa2710eb989afb09b5
Mariusz  Futerski <mfuterski@yahoo.com>
2023-09-28 13:34:36 +0000

    dassda

--- .env.example.sh
+++ .env.example.sh
@@ -1,6 +1,6 @@
 
 
-PROJECT_ROOT='/workspace/terraform-begginer-bootcamp-2023'
+PROJECT_ROOT='/workspace/terraform-beginer-bootcamp-2023'
 $ AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
 $ AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 $ AWS_DEFAULT_REGION=eu-west-2
